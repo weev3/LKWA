72f383ee17eabeb63fec6ea16b5e88149e0bf715
weev3 <weev3@outlook.com>
2020-01-07 22:00:31 +0630

    Update sidebar.php

--- ssrf/sidebar.php
+++ ssrf/sidebar.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>PHAR Deserialization</title>
+  <title>SSRF</title>
   <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
